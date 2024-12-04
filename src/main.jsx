41712b148c2c2fe72cf0ee475c29aef9cbef7e05
TranCongViet <trancongviet990ntt@gmail.com>
2024-12-04 19:38:41 +0700

    hashbrowsers

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './assets/styles/index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </BrowserRouter>
+  </HashRouter>
 )
